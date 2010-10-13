c3619a8897e2c427f36f1c39328445adb43bee22
Tim Tyrrell <email@timtyrrell.net>
2010-10-12 22:41:42 -0500

    functioning procedural code

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source "http://rubygems.org"
 gem "patron"
 gem "mail"
+gem "json_pure"
 
--- emailbot.rb
+++ emailbot.rb
@@ -1,4 +1,6 @@
 require 'mail'
+require 'patron'
+require 'json/pure'
 
 Mail.defaults do
    retriever_method :pop3, { :address             => "pop.gmail.com",
@@ -8,8 +10,36 @@ Mail.defaults do
                              :enable_ssl          => true }
  end
 
-mail = Mail.first
-puts mail.body.decoded
+mail = Mail.last
+mail_command = mail.body.decoded
 
-#I know this is shit, I was just doing some testing
+sess = Patron::Session.new
+sess.timeout = 10
+sess.base_url = "http://api.gowalla.com"
+sess.headers['X-Gowalla-API-Key'] = ''
+sess.headers['Accept'] = 'application/json'
+sess.headers['Content-Type'] = 'application/json'
+
+resp = sess.get("/spots/2380289/events")
+
+if resp.status < 400
+  result =  JSON.parse resp.body
+
+  users = result["activity"]
+  users.collect! do |returned_users|
+    "#{returned_users["user"]["last_name"]}, #{returned_users["user"]["first_name"]}"
+  end
+#  if mail_command == 'all'
+#    puts "Return #{users.length} users"
+#  elsif mail_command.is_a?(Integer)
+#    random_users = users.shuffle!.first(mail_command)
+#    puts "Return #{random_users.length} users"
+#  else
+#    puts "Unsuccessful Gowalla API call with #{mail_command}."
+#  end
+    random_users = users.shuffle!.first(mail_command.to_i)
+    puts "Return #{random_users.length} users"
+else
+  puts resp.status
+end
 
